d0806e3cb5b499756a06df5b2fb4ccae24cf36c4
jgluna <jgluna85@gmail.com>
2015-10-26 22:00:48 -0500

    Created SpringBoot App Created Controller Interface

--- src/main/java/com/github/jgluna/ws/App.java
+++ src/main/java/com/github/jgluna/ws/App.java
@@ -1,10 +1,17 @@
 package com.github.jgluna.ws;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableWebMvc
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class,args);
     }
 }
--- src/main/java/com/github/jgluna/ws/controller/EffectsController.java
+++ src/main/java/com/github/jgluna/ws/controller/EffectsController.java
@@ -1,7 +1,22 @@
 package com.github.jgluna.ws.controller;
 
+import com.github.jgluna.ws.model.EffectsRequestWrapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Part;
+import retrofit.mime.TypedFile;
 
 @RestController
-public class EffectsController {
+public class EffectsController implements EffectsControllerInterface {
+
+    @RequestMapping(value = BASE_PATH, method = RequestMethod.POST)
+    public Void applyEffect(@Part(IMAGE_PARAMETER) TypedFile image,
+                            @Part(WRAPPER_PARAMETER) EffectsRequestWrapper effectsWrapper,
+                            Callback<Response> callback) {
+        System.out.println("Recibi un rikues");
+        return null;
+    }
 }
